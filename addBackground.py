f316901d319ddba0c0f1d200db8cdba879ec4b08
Dion Lao <dion.lao@uqconnect.edu.au>
2018-10-20 19:08:08 +0800

    Fixed folder not reached issue

--- addBackground.py
+++ addBackground.py
@@ -2,7 +2,7 @@ def addBackground():
     from PIL import Image
     foreground = Image.open('filtered.png', 'r')
     img_w, img_h = foreground.size
-    background = Image.open('../space_background_still2.gif','r')
+    background = Image.open('background/space_background_still2.gif','r')
     bg_w, bg_h = background.size
     offset = ((bg_w - img_w) // 2, (bg_h - img_h) // 2)
     background.paste(foreground, offset)
